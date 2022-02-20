12dfb907d61659dbc8a35a11bd5496a8c6668eb2
Geraldoeze <egerald344@gmail.com>
2022-02-21 00:02:29 +0100

    styles

--- components/views.js
+++ components/views.js
@@ -16,12 +16,12 @@ const Views = ({ data }) => {
   
         return(
 
-      <div key={id} className='flex flex-wrap m-auto my-4 bg-zinc-900 rounded-xl w-11/12 overflow-x-auto'>
+      <div key={id} className='flex flex-wrap m-auto my-4 bg-zinc-900 rounded-xl w-11/12 overflow-x-auto :'>
           <div className='w-32'>
            <img src="https://image.shutterstock.com/image-vector/flat-street-map-direction-gps-600w-1661529616.jpg"
-            alt='map' className='rounded-xl m-4 w-20'/>
+            alt='map' className='rounded-xl m-4 w-20 md:w-40 lg:w-80'/>
           </div>
-          <div className='sm:w-40 text-sm my-4 md:text-xl w-60'>
+          <div className='w-40 text-sm my-4 ml-2 md:text-xl ml-10 mr-6 w-40 lg:w-6/12 mx-20'>
             <p>Ride Id: {val.id}</p>
             <p>Origin Station: {val.origin_station_code}</p>
             <p>station_path: [{ stationArray}]</p>
@@ -29,8 +29,8 @@ const Views = ({ data }) => {
             <p>Distance: {val.distance}</p>
              
           </div>
-          <div className='w-10 my-4'>
-            <ul className='flex text-center'>
+          <div className='w-10 my-4 md:w-20 lg:w-60'>
+            <ul className='flex text-center md:ml-au-auto' >
               <li className='mx-2 px-1 rounded-xl bg-black'>City Name</li>
               <li className='mx-2 px-1 rounded-xl bg-black'>State Name</li>
             </ul>
